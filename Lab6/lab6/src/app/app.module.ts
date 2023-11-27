79d7e9029e2afcd290bd2f4d49a506570c2cd176
YehorChaliy <99450519+YehorChaliy@users.noreply.github.com>
2023-11-27 16:56:09 +0300

    lab6

---
+++ Lab6/lab6/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicStorageModule } from '@ionic/storage-angular';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { UserDetailsModal } from './popup.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FavoritesComponent } from './favorites.component';
+
+@NgModule({
+  declarations: [AppComponent, UserDetailsModal, FavoritesComponent],
+  imports: [BrowserModule, HttpClientModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
